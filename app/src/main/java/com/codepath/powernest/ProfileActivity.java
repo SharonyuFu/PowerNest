d6f4249593e0c077488cdf605b91d2c601c7023b
Zen <zuzen324@gmail.com>
2017-07-30 09:07:00 +0800

    add profile

--- app/src/main/java/com/codepath/powernest/ProfileActivity.java
+++ app/src/main/java/com/codepath/powernest/ProfileActivity.java
@@ -1,13 +1,31 @@
 package com.codepath.powernest;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ImageView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private ImageView ivProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        toolbar = (Toolbar)findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("My Profile");
+
+
+//        ivProfile = (ImageView)findViewById(R.id.ivProfile);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_person, menu);
+        return true;
     }
 }

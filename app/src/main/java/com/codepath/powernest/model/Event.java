613997052c50f9230db8ee3efdd532fbec98a81b
Zen <zuzen324@gmail.com>
2017-07-30 09:57:18 +0800

    modify event content

--- app/src/main/java/com/codepath/powernest/model/Event.java
+++ app/src/main/java/com/codepath/powernest/model/Event.java
@@ -62,7 +62,7 @@ public class Event {
 
     public static List<Event> getEvents() {
         List<Event> events = new ArrayList<>();
-        events.add(new Event(R.drawable.daan_park,"Daan Park Wednesday", "Daan Ruuning Group", true,"2017-07-29","19:00","Easy Training"));
+        events.add(new Event(R.drawable.daan_park,"Party Voting", "Daan Ruuning Group", true,"2017-07-29","19:00","Easy Training"));
         events.add(new Event(R.drawable.odiac,"Odiac Random Day", "Odiac Run", true,"2017-07-30","18:00","Other"));
         events.add(new Event(R.drawable.comboun,"Combo Rush Routine", "Combo Run", true,"2017-08-01","19:00","Interval Training"));
         events.add(new Event(R.drawable.daan_park,"Daan Park LateNight", "Daan Ruuning Group", true,"2017-08-01","21:00","Easy Training"));
